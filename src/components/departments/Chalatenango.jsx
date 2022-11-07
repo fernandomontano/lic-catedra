178988192652deb89436e631cd1230d35587d61c
Andrea Velásquez <67344634+andreavvel@users.noreply.github.com>
2022-11-07 00:03:48 -0600

    updated departments

--- src/components/departments/Chalatenango.jsx
+++ src/components/departments/Chalatenango.jsx
@@ -1,39 +1,67 @@
 import React from "react";
 import img from "../../../public/chalatenango.jpg";
-
+import { UilUserArrows } from "@iconscout/react-unicons";
+import { UilUsersAlt } from "@iconscout/react-unicons";
+import { UilDollarAlt } from "@iconscout/react-unicons";
 import "../../styles/image.css";
 
 export default function Chalatenango() {
   return (
-    <div className="container container-fluid h-100">
-      <section className="mx-5 rounded d-flex align-items-center h-100 ">
-        <div className="d-flex flex-column rounded flex-wrap bg-light">
-          <div className="d-flex justify-content-around align-items-center flex-wrap">
+    <div className="container container-fluid mt-3">
+    <section className="rounded d-flex align-items-center justify-content-center flex-wrap h-100 w-100">
+      <div className="bg-light rounded w-75 d-flex flex-column justify-content-center ">
+        <div className="p-5 d-flex flex-wrap justify-content-around align-items-center">
+          <img
+            src={img}
+            alt="Teatro"
+            className="img-fluid rounded"
+            width="400px"
+          />
+          <div className="text-center mt-3 lh-lg responsive-width">
+            <h3>Chalatenango</h3>
+            <h4 className="text-danger">"Valle de aguas y arena"</h4>
             <div>
-              <img src={img} className="rounded"></img>
-            </div>
-            <div className="text-center d-flex flex-column responsive-width p-4">
-              <h1 className="mt-2 ">Chalatenango</h1>
-              <div className="text-danger">"Valle de aguas y arena"</div>
-              <div className="lh-lg">
-                El nombre Chalatenango es de origen náhuat, y proviene de chal,
+            El nombre Chalatenango es de origen náhuat, y proviene de chal,
                 shal (arena); at, (agua): río; y tenango (valle).
                 Etimológicamente, valle de aguas y arena. Chalatenango se
                 asienta en el borde sur de la Sierra Madre a su paso por
-                Centroamérica. Chalatenango se convirtió en un importante centro
-                del cultivo del añil y se mantuvo como tal durante todo el
-                período colonial, esto ocasionó que el municipio junto a todo el
-                departamento se posicionara como un productor añilero.
+                Centroamérica.
+            </div>
+          </div>
+        </div>
+        <div className="d-flex justify-content-around">
+          <div className="line bg-secondary rounded-1"></div>
+        </div>
+        <div className="px-5 py-3">
+          <h3>General</h3>
+          <div className="">
+            <div className="d-flex justify-content-between align-items-center gap-2 flex-wrap my-2 center-card">
+              <div className="bg-secondary rounded">
+                <div className="p-3 text-center">
+                  <UilUserArrows size="50" color="#f0f5fa" />
+                  <h5>Gentilicio</h5>
+                  <p>"Chalateco(a)"</p>
+                </div>
+              </div>
+              <div className="bg-secondary rounded">
+                <div className="p-3 text-center">
+                  <UilUsersAlt size="50" color="#f0f5fa" />
+                  <h5>Población</h5>
+                  <p>Total 275, 100 hab.</p>
+                </div>
+              </div>
+              <div className="bg-secondary rounded">
+                <div className="p-3 text-center">
+                  <UilDollarAlt size="50" color="#f0f5fa" />
+                  <h5>PIB</h5>
+                  <p>Total (2013) 161,645 US$</p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="d-flex flex-column justify-content-around p-2">
-            <div className="line bg-secondary rounded mb-3"></div>
-            <div className="d-flex justify-content-around"></div>
-          </div>
         </div>
-      </section>
-      <div></div>
-    </div>
-  );
+      </div>
+    </section>
+    <div></div>
+  </div>);
 }
